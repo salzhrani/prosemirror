ca79b103043c90379f7d9fc35704ea8a0f9f220d
Samy Alzahrani <samy@jehazy.com>
2016-09-20 12:51:42 +0300

    ci

--- src/test/run-ci.js
+++ src/test/run-ci.js
@@ -2,74 +2,70 @@ let webdriver = require("selenium-webdriver")
 
 function testBrowser(caps) {
 	return new Promise((resolve, reject) => {
-		let browser = new webdriver.Builder()
+    let browser = new webdriver.Builder()
 		.usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
 		.withCapabilities(Object.assign({}, caps, {
 			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
 			build: process.env.TRAVIS_BUILD_NUMBER,
 			username: process.env.SAUCE_USERNAME,
-			accessKey: process.env.SAUCE_ACCESS_KEY,
-		})).build();
+			accessKey: process.env.SAUCE_ACCESS_KEY
+		})).build()
 		browser.get("http://localhost:8080/test")
 		function checkIsDone() {
 			browser.executeScript('return JSON.stringify(window.done)')
 			.then((res) => {
-				res = JSON.parse(res);
+				res = JSON.parse(res)
 				if (res === true) {
 					browser.executeScript('return JSON.stringify(window.results)')
 					.then((results) => {
-						results = JSON.parse(results);
-						browser.quit();
+						results = JSON.parse(results)
+						browser.quit()
 						if (results.failed < 1) {
-							console.log("Ran " + results.passed + " tests on (" + caps.browserName + ', ' + caps.platform + ') all passed');
-							resolve()
-							// process.exit(0);
+							resolve("Ran " + results.passed + " tests on (" + caps.browserName + ', ' + caps.platform + ') all passed')
 						} else {
-							console.log("Ran " + (results.passed + results.failed) + " tests on (" + caps.browserName + ', ' + caps.platform + '), ' + results.failed + ' failed.\n');
-							console.log(results.errors.join('\n'))
-							resolve()
-							// process.exit(1);
+							reject("Ran " + (results.passed + results.failed) + " tests on (" + caps.browserName + ', ' + caps.platform + '), ' + results.failed + ' failed.\n' + results.errors.join('\n'))
 						}
 					})
 				} else {
 					setTimeout(function() {
-						checkIsDone();
-					}, 1000);
+						checkIsDone()
+					}, 1000)
 				}
 			})
 		}
 		checkIsDone()
-	});
+	})
 }
-
+Promise.all([
 testBrowser({
 	browserName: 'chrome',
 	platform: 'OS X 10.11',
-	version: 'beta',
-})
-.then(() => testBrowser({
+	version: 'beta'
+}),
+testBrowser({
 	browserName: 'chrome',
 	platform: 'Windows 10',
-	version: 'beta',
-}))
-.then(() => testBrowser({
+	version: 'beta'
+}),
+testBrowser({
 	browserName: 'chrome',
 	platform: 'Linux',
-	version: '48.0',
-}))
-.then(() => testBrowser({
+	version: '48.0'
+}),
+testBrowser({
 	browserName: 'safari',
 	platform: 'OS X 10.11',
-	version: '9.0',
-}))
-.then(() => testBrowser({
+	version: '9.0'
+}),
+testBrowser({
 	browserName: 'firefox',
 	platform: 'OS X 10.11',
-	version: 'beta',
-}))
-.then(() => testBrowser({
+	version: 'beta'
+}),
+testBrowser({
 	browserName: 'MicrosoftEdge',
 	platform: 'Windows 10',
-	version: '13.10586',
-}))
-.then(() => process.exit(0), () => process.exit(1))
+	version: '13.10586'
+})])
+.then(() => process.exit(0), () => process.exit(1));
+
